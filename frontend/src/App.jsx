37ccb8be1764155c6951c89538d250842e9d645f
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2025-07-29 19:13:52 +0500

    app structure completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,114 @@
-import { useState } from "react";
 import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape";
+import DashboardPage from "./components/pages/DashboardPage";
+import SignUpPage from "./components/pages/SignUpPage";
+import LoginPage from "./components/pages/LoginPage";
+import ResetPasswordPage from "./components/pages/ResetPasswordPage";
+import ForgotPasswordPage from "./components/pages/ForgotPassword";
+import EmailVerificationPage from "./components/pages/EmailVerificationPage";
+import { useAuthStore } from "./store/authStore";
+import LoadingSpinner from "./components/LoadingSpinner";
+
+import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+
+
+ // protect routes that require authentication
+const ProtectedRoute = ({ children }) => {
+	const { isAuthenticated, user } = useAuthStore();
+
+	if (!isAuthenticated) {
+		return <Navigate to='/login' replace />;
+	}
+
+	if (!user.isVerified) {
+		return <Navigate to='/verify-email' replace />;
+	}
+
+	return children;
+};
+
+
+// redirect authenticated users to the home page
+const RedirectAuthenticatedUser = ({ children }) => {
+	const { isAuthenticated, user } = useAuthStore();
+
+	if (isAuthenticated && user.isVerified) {
+		return <Navigate to='/' replace />;
+	}
+
+	return children;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <div
-      className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900
-    to-emerald-900 flex items-center justify-center overflow-hidden relative"
-    >
-      <FloatingShape
-        color="bg-green-500"
-        size="w-64 h-64"
-        top="-5%"
-        left="10%"
-        delay={0}
-      />
-      <FloatingShape
-        color="bg-emerald-500"
-        size="w-48 h-48"
-        top="70%"
-        left="80%"
-        delay={5}
-      />
-      <FloatingShape
-        color="bg-lime-500"
-        size="w-32 h-32"
-        top="40%"
-        left="-10%"
-        delay={2}
-      />
+const { isCheckingAuth, checkAuth } = useAuthStore();
 
-      <Routes>
-        <Route/>
+	useEffect(() => {
+		checkAuth();
+	}, [checkAuth]);
 
-      </Routes>
-    </div>
-  );
+	if (isCheckingAuth) return <LoadingSpinner />;
+
+ return (
+		<div
+			className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'
+		>
+			<FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<ProtectedRoute>
+							<DashboardPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path='/signup'
+					element={
+						<RedirectAuthenticatedUser>
+							<SignUpPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				<Route
+					path='/login'
+					element={
+						<RedirectAuthenticatedUser>
+							<LoginPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				<Route path='/verify-email' element={<EmailVerificationPage />} />
+				<Route
+					path='/forgot-password'
+					element={
+						<RedirectAuthenticatedUser>
+							<ForgotPasswordPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+
+				<Route
+					path='/reset-password/:token'
+					element={
+						<RedirectAuthenticatedUser>
+							<ResetPasswordPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				{/* catch all routes */}
+				<Route path='*' element={<Navigate to='/' replace />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
 
 export default App;
