84b638282f3832437c0ec057b214d24a59df2aba
Muhammad Arslan <muhammad.arslan9799@gmail.com>
2025-08-09 18:25:59 +0500

    Updated CLIENT_URL

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import axios from 'axios';
 
 
-const API_URL = `${import.meta.env.VITE_API_URL}`;
+const API_URL = `${import.meta.env.CLIENT_URL}`;
  console.log(API_URL)
 
 export const useAuthStore = create((set) => {
